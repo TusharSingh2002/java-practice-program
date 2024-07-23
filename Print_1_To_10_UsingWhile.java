d3f020055e95441a4fb9f758d15798daab035a03
TusharSingh2002 <145903786+TusharSingh2002@users.noreply.github.com>
2024-07-23 23:04:06 +0530

    Add files via upload

---
+++ Print_1_To_10_UsingWhile.java
@@ -0,0 +1,21 @@
+
+
+public class Print_1_To_10_UsingWhile 
+{
+	public static void main(String args[])
+	{
+
+		int i=1;
+
+	
+		System.out.println("Output is : ");
+
+		
+		while(i<=10)	
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
+
