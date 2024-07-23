7a4882afb24d96e951d99a9f96dbdd6c499f993a
TusharSingh2002 <145903786+TusharSingh2002@users.noreply.github.com>
2024-07-23 23:02:34 +0530

    Add files via upload

---
+++ adding.java
@@ -0,0 +1,12 @@
+public class adding {
+    public static void main(String[] args) {
+        
+              
+              int first = 5;
+             double second = 7.5;
+          
+              // add two numbers
+              double sum = first + second;
+              System.out.println(first + " + " + second + " = "  + sum);
+            }
+        }
